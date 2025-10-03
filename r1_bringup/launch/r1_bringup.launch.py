bb700e3014996f208ba6b6e5c9ed09620dfe44ea
yudai <yudai.yy0804@gmail.com>
2025-10-03 17:27:30 +0900

    add: launch書いた

--- r1_bringup/launch/r1_bringup.launch.py
+++ r1_bringup/launch/r1_bringup.launch.py
@@ -1,8 +1,16 @@
 import os
+import time
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import (
+    AnyLaunchDescriptionSource,
+    PythonLaunchDescriptionSource,
+)
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -12,7 +20,12 @@ def generate_launch_description():
     # パラメータファイルのフルパスを作成
     param_file = os.path.join(pkg_dir, "config", "r1_machine_config.yaml")
 
-    # TODO: 次はros2_socketcanとsabacanのlaunchを書くところから。
+    ps4_node = Node(
+        package="joy",
+        executable="joy_node",
+        name="joy_node",
+        arguments=["--ros-args", "--log-level", "warn"],
+    )
 
     r1_mecanum_node = Node(
         package="r1_machine",
@@ -26,7 +39,39 @@ def generate_launch_description():
         package="r1_state_machine",
         executable="r1_state_machine_node",
         name="r1_state_machine_node",
+        parameters=[param_file],
         arguments=["--ros-args", "--log-level", "warn"],
     )
 
-    return LaunchDescription([r1_mecanum_node, r1_state_machine_node])
+    sabacan_robomas_node1 = Node(
+        package="sabacan",
+        executable="sabacan_robomasv2_node",
+        name="sabacan_robomas_node1",
+        parameters=[param_file],
+        arguments=["--ros-args", "--log-level", "warn"],
+    )
+
+    socket_can_bridge_launch = IncludeLaunchDescription(
+        AnyLaunchDescriptionSource(
+            [
+                PathJoinSubstitution(
+                    [
+                        FindPackageShare("ros2_socketcan"),
+                        "launch",
+                        "socket_can_bridge.launch.xml",
+                    ]
+                )
+            ]
+        ),
+        launch_arguments={"interface": "can0"}.items(),
+    )
+
+    return LaunchDescription(
+        [
+            ps4_node,
+            r1_mecanum_node,
+            r1_state_machine_node,
+            sabacan_robomas_node1,
+            socket_can_bridge_launch,
+        ]
+    )
