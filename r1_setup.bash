27342c49c8feaed14cc8173a9ceb667491dc7dbc
yudai <yudai.yy0804@gmail.com>
2026-03-29 19:08:12 +0900

    update

--- r1_setup.bash
+++ r1_setup.bash
@@ -1,13 +1,24 @@
 echo "========== R1 setup start =========="
-cd $HOME/ros2_ws
+
+cd "$HOME/ros2_ws"
 source install/setup.bash
+
 sudo cpupower frequency-set -g performance
-# TODO: 他のデバイス名でも対応できるようにする
-sudo chmod 666 /dev/ttyUSB0
-echo 1 | sudo tee /sys/bus/usb-serial/devices/ttyUSB0/latency_timer
-sudo chmod 666 /dev/ttyACM0
+
+IMU_DEV="/dev/serial/by-id/usb-FTDI_FT230X_Basic_UART_D20K2C1C-if00-port0"
+LIDAR_DEV="/dev/ttyACM0"
+
+# by-id -> 実体の ttyUSBx を取得
+IMU_TTY="$(basename "$(readlink -f "$IMU_DEV")")"
+
+sudo chmod 666 "$IMU_DEV"
+echo 1 | sudo tee "/sys/bus/usb-serial/devices/$IMU_TTY/latency_timer"
+
+sudo chmod 666 "$LIDAR_DEV"
+
 echo "---------- IMU setup OK ----------"
+
 sudo ./src/gakurobo2026_common/can_setup.bash
+
 echo "--------- USB2CAN setup OK ----------"
 echo "========= R1 setup success =========="
-# ros2 launch ros2_socketcan socket_can_bridge.launch.xml interface:=can0
