176a15c20f2fa353b8001f8e6142f028d0487500
yudai <yudai.yy0804@gmail.com>
2026-04-03 01:34:51 +0900

    シーケンス変更

--- r1_main/src/r1_main_node.cpp
+++ r1_main/src/r1_main_node.cpp
@@ -914,12 +914,20 @@ void R1MainNode::manual_mode2_collect_pole_task(void)
     spear_u2_valve(false);
     step++;
   } else if (step == 9) {
+    spear_d1_valve(true);
+    spear_d2_valve(true);
+    step++;
+  } else if (step == 10) {
     spear1(SPEAR1_NORMAL_POS);
     spear2(SPEAR2_NORMAL_POS);
     spear_pitch1(SPEAR_PITCH1_NORMAL_ANGLE);
     spear_pitch2(SPEAR_PITCH2_NORMAL_ANGLE);
     step++;
-  } else if (step == 10) {
+  } else if (step == 11) {
+    spear_d1_valve(false);
+    spear_d2_valve(false);
+    step++;
+  } else if (step == 12) {
     spear_y(SPEAR_Y_NORMAL_POS);
     RCLCPP_INFO(this->get_logger(), "pole collect task completed");
     step = 1;
