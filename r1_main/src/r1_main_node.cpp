a594582dbee1d7f86ed5c5afd0a0db792d398398
yudai <yudai.yy0804@gmail.com>
2026-04-14 23:55:50 +0900

    ポール回収のときにrollを回転しないようにしてみた

--- r1_main/src/r1_main_node.cpp
+++ r1_main/src/r1_main_node.cpp
@@ -1681,31 +1681,28 @@ void R1MainNode::manual_mode2_collect_pole_task(void)
     spear2_pos_ref(SPEAR2_COLLECT2_POS);
     step++;
   } else if (step == 7) {
-    spear_roll_pos_ref(SPEAR_ROLL_NORMAL_ANGLE);
-    step++;
-  } else if (step == 8) {
     spear1_pos_ref(SPEAR1_COLLECT3_POS);
     spear2_pos_ref(SPEAR2_COLLECT3_POS);
     step++;
-  } else if (step == 9) {
+  } else if (step == 8) {
     spear_u1_valve(false);
     spear_u2_valve(false);
     step++;
-  } else if (step == 10) {
+  } else if (step == 9) {
     spear_d1_valve(true);
     spear_d2_valve(true);
     step++;
-  } else if (step == 11) {
+  } else if (step == 10) {
     spear1_pos_ref(SPEAR1_NORMAL_POS);
     spear2_pos_ref(SPEAR2_NORMAL_POS);
     spear_pitch1_pos_ref(SPEAR_PITCH1_NORMAL_ANGLE);
     spear_pitch2_pos_ref(SPEAR_PITCH2_NORMAL_ANGLE);
     step++;
-  } else if (step == 12) {
+  } else if (step == 11) {
     spear_d1_valve(false);
     spear_d2_valve(false);
     step++;
-  } else if (step == 13) {
+  } else if (step == 12) {
     spear_y_pos_ref(SPEAR_Y_NORMAL_POS);
     RCLCPP_INFO(this->get_logger(), "pole collect task completed");
     step = 1;
