d05850a9a6c4a3572d1e0ccbba7ac1e3f5c9879c
yudai <yudai.yy0804@gmail.com>
2026-04-29 13:44:02 +0900

    update

--- scripts/r1_setup.bash
+++ scripts/r1_setup.bash
@@ -14,7 +14,7 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=$XDG_RUNTIME_DIR/bus
 
 sudo cpupower frequency-set -g performance
 
-IMU_DEV="/dev/serial/by-path/pci-0000:00:14.0-usb-0:1.2.4:1.0-port0"
+IMU_DEV="/dev/serial/by-path/pci-0000:00:14.0-usb-0:1.4.4:1.0-port0"
 LIDAR1_DEV="/dev/serial/by-path/pci-0000:00:14.0-usb-0:1.4:1.0"
 LIDAR2_DEV="/dev/serial/by-path/pci-0000:00:14.0-usb-0:1.1:1.0"
 ARUCO_DEV="/dev/serial/by-path/pci-0000:00:14.0-usb-0:1.2.1:1.0-port0"
