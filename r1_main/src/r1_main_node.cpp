583089235c4b789300a26179bcaab48faada3973
yudai <yudai.yy0804@gmail.com>
2026-04-29 12:08:55 +0900

    update

--- r1_main/src/r1_main_node.cpp
+++ r1_main/src/r1_main_node.cpp
@@ -2162,6 +2162,8 @@ void R1MainNode::manual_mode2_collect_pole_task(void)
   int & step = manual_mode2_collect_pole_task_step_;
   RCLCPP_INFO(this->get_logger(), "manual_mode2_collect_pole_task step: %d", step);
   if (step == 1) {
+    spear_y_pos_ref(0.0);
+    spear_roll_pos_ref(0.0);
     spear_hand1_valve(true);
     spear_hand2_valve(true);
     spear_hand_push_valve(true);
